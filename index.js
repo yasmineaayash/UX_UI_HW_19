20ca62abebd24c1f637db3b84dc43fe089d8d789
yasmineaayash <119134112+yasmineaayash@users.noreply.github.com>
2023-05-08 18:00:04 -0400

    May 8th, 5:30 pm

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("test1");
+$(".navlink").hover(function(){
+    $(this).css("color", "black");
+  },
+  function(){
+    $(this).css("color", "#DFCFD2");
+  });
+
+  $(".workimages").hover(function(){
+    $(this).css("width","75%");
+  },
+  function(){
+    $(this).css("width", "70%");
+  }); 
+
+  $(".navlink").hover(function(){
+    $(this).css("color", "black");
+  },
+  function(){
+    $(this).css("color", "#DFCFD2");
+  });
+  
